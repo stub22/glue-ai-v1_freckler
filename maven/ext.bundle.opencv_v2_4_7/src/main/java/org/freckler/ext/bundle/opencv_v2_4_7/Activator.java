51f955cf44fc22f215398eaa2074570ec25b527d
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2014-01-13 19:52:16 +0000

    New bundle.

---
+++ maven/ext.bundle.opencv_v2_4_7/src/main/java/org/freckler/ext/bundle/opencv_v2_4_7/Activator.java
@@ -0,0 +1,16 @@
+package org.freckler.ext.bundle.opencv_v2_4_7;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+    public void start(BundleContext context) throws Exception {
+        // TODO add activation code here
+    }
+
+    public void stop(BundleContext context) throws Exception {
+        // TODO add deactivation code here
+    }
+
+}
